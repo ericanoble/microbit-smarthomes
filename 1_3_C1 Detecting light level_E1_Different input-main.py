d0adf73088f68e36ba1e45a04ceeb3ad3365e935
ericanoble <erica.noble1@decyp.tas.gov.au>
2026-09-08 18:38:16 +1000

    Update 1_3_C1 Detecting light level_E1_Different input-main.py

--- 1_3_C1 Detecting light level_E1_Different input-main.py
+++ 1_3_C1 Detecting light level_E1_Different input-main.py
@@ -1,4 +1,6 @@
-# Instead of button input use input from the touch logo, accelerometer, microphone, compass, light sensor or temperature sensor
+# Instead of button input use input from the touch logo,
+# accelerometer, microphone, compass, light sensor
+# or temperature sensor
 from microbit import *
 while True:
     if pin_logo.is_touched():
