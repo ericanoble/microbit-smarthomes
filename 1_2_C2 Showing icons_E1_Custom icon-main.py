474ab34ffa83e16522e29b31a0b1b5b8b7e38d69
ericanoble <erica.noble1@decyp.tas.gov.au>
2026-09-10 12:42:45 +1000

    Update 1_2_C2 Showing icons_E1_Custom icon-main.py

--- 1_2_C2 Showing icons_E1_Custom icon-main.py
+++ 1_2_C2 Showing icons_E1_Custom icon-main.py
@@ -1,6 +1,7 @@
 # MicroPython DIY Images
 # https://microbit-micropython.readthedocs.io/en/latest/tutorials/images.html#diy-images
-# When run, the micro:bit should display an old-fashioned “Blue Peter” sailing ship with the masts dimmer than the boat’s hull.
+# When run, the micro:bit should display an old-fashioned “Blue Peter” sailing ship
+# with the masts dimmer than the boat’s hull.
 from microbit import *
 while True:
     if button_a.is_pressed():
